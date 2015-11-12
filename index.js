0c48c161743523903d532ca3b1a8a59583363a1c
Samfox2 <Samfox2@users.noreply.github.com>
2015-11-12 10:59:23 +0100

    Update index.js
    added service switch (not testet, functionality hast to be validated)

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ module.exports = function (homebridge) {
     Characteristic = homebridge.hap.Characteristic;
     homebridge.registerAccessory("homebridge-domotiga", "Domotiga", Domotiga);
 }
-
-
 //Get data from config file 
 function Domotiga(log, config) {
     this.log = log;
@@ -20,11 +18,11 @@ function Domotiga(log, config) {
         valueHumidity: config.valueHumidity,
         valueBattery: config.valueBattery,
         valueContact: config.valueContact || 1,
+        valueSwitch: config.valueSwitch || 1,
         name: config.name || NA,
         lowbattery: config.lowbattery
     };
 }
-
 Domotiga.prototype = {
     identify: function (callback) {
         this.log("Identify requested!");
@@ -36,42 +34,69 @@ Domotiga.prototype = {
                 {
                     jsonrpc: "2.0",
                     method: "device.get",
-                    params: {"device_id": that.config.device},
+                    params: { "device_id": that.config.device },
                     id: 1
                 }, function (err, data) {
 
-            if (err) {
-                that.log("Sorry err: ", err);
-                callback(err);
-            }
-            else {
-                item = Number(deviceValueNo) - 1;
-                //that.log("data.result:", data.result);
-                //that.log( "data.result[values][0][value]", data.result[values][0][value]);
-                i = 0;
-                for (key1 in data.result) {
-                    if (i == 37) {
-                        //that.log("key1 ", i, key1, "values[key1]", values[key1]);
-                        j = 0;
-                        for (key2 in data.result[key1]) {
-                            if (j == item) {
-                                //that.log("key2 ", j, key2, "values[key1][key2]", values[key1][key2]);
-                                k = 0;
-                                for (key3 in data.result[key1][key2]) {
-                                    if (k == 17) {
-                                        //that.log("key3 ", k, key3, "data.result[key1][key2][key3]", data.result[key1][key2][key3]);
-                                        callback(null, data.result[key1][key2][key3]);
+                    if (err) {
+                        that.log("Sorry err: ", err);
+                        callback(err);
+                    }
+                    else {
+                        item = Number(deviceValueNo) - 1;
+                        //that.log("data.result:", data.result);
+                        //that.log( "data.result[values][0][value]", data.result[values][0][value]);
+                        i = 0;
+                        for (key1 in data.result) {
+                            if (i == 37) {
+                                //that.log("key1 ", i, key1, "values[key1]", values[key1]);
+                                j = 0;
+                                for (key2 in data.result[key1]) {
+                                    if (j == item) {
+                                        //that.log("key2 ", j, key2, "values[key1][key2]", values[key1][key2]);
+                                        k = 0;
+                                        for (key3 in data.result[key1][key2]) {
+                                            if (k == 17) {
+                                                //that.log("key3 ", k, key3, "data.result[key1][key2][key3]", data.result[key1][key2][key3]);
+                                                callback(null, data.result[key1][key2][key3]);
+                                            }
+                                            ++k;
+                                        }
                                     }
-                                    ++k;
+                                    ++j;
                                 }
                             }
-                            ++j;
+                            ++i;
                         }
                     }
-                    ++i;
-                }
-            }
-        });
+                });
+    },
+    setValueToDomotiga: function (deviceValueNo, value, callback) {
+        var that = this;
+
+        //dbg: Message format:
+        // json='application/json'
+        // params='"params": {"device_id": 52, "valuenum": 1, "value": "Off"}'
+        // request='{"jsonrpc": "2.0", "method": "device.set", '${params}', "id": 1}'
+        // curl -sS -X POST -H "Content-Type: $json" -H "Accept: $json" -d "$request" localhost:9090
+
+        //todo  
+        JSONRequest('http://' + that.config.host + ':' + that.config.port,
+                {
+                    jsonrpc: "2.0",
+                    method: "device.set",
+                    params: { "device_id": that.config.device, "valuenum": deviceValueNo, "value": value },
+                    id: 1
+                }, function (err, data) {
+                    //that.log("data:", data);
+                    if (err) {
+                        that.log("Sorry err: ", err);
+                        callback(err);
+                    }
+                    else {
+                        callback(NULL,data)
+                    }
+                });
     },
     getCurrentRelativeHumidity: function (callback) {
         var that = this;
@@ -147,6 +172,45 @@ Domotiga.prototype = {
             }
         }.bind(this));
     },
+    getSwitchOn: function (callback) {
+        var that = this;
+        that.log("getting SwitchState for " + that.config.name);
+        that.getValueFromDomotiga(that.config.valueSwitch, function (error, result) {
+            if (error) {
+                that.log('getSwitchOn GetValue failed: %s', error.message);
+                callback(error);
+            } else {
+                if (result.toLowerCase() == "on") {
+                    callback(null, 1);
+                }
+                else {
+                    callback(null, 0);
+                }
+            }
+        }.bind(this));
+    },
+    setSwitchOn: function (switchOn, callback) {
+        var that = this;
+        var binaryState = switchOn ? On : Off;
+        that.log("Setting SwitchState for '%s' to %s", that.config.name, binaryState);
+
+        var callbackWasCalled = false;
+
+        that.setValueToDomotiga(that.config.valueSwitch, binaryState, function (err, result) {
+            if (callbackWasCalled) {
+                that.log("WARNING: setValueToDomotiga called its callback more than once! Discarding the second one.");
+            }
+            callbackWasCalled = true;
+            if (!err) {
+                that.log("Successfully set switch state on the '%s' to %s", that.config.name, binaryState);
+                callback(null);
+            }
+            else {
+                that.log("Error setting switch state to %s on the '%s'", binaryState, that.config.name);
+                callback(err);
+            }
+        }.bind(this));
+    },
     getServices: function () {
         // you can OPTIONALLY create an information service if you wish to override
         // the default values for things like serial number, model, etc.
@@ -182,8 +246,7 @@ Domotiga.prototype = {
 
             return [informationService, controlService];
         }
-
-        if (this.config.service == "Contact") {
+        else if (this.config.service == "Contact") {
 
             informationService
                     .setCharacteristic(Characteristic.Manufacturer, "Contact Manufacturer")
@@ -207,6 +270,16 @@ Domotiga.prototype = {
                         .addCharacteristic(Characteristic.StatusLowBattery)
                         .on('get', this.getLowBatteryStatus.bind(this));
             }
+            return [informationService, controlService];
+        }
+        else if (this.config.service == "Switch") {
+            var controlService = new Service.Switch(this.config.name);
+
+            controlService
+              .getCharacteristic(Characteristic.On)
+              .on('get', this.getSwitchOn.bind(this))
+              .on('set', this.setSwitchOn.bind(this));
+
             return [informationService, controlService];
         }
     }
