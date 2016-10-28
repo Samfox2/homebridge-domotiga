e3b646b201f0254a78a24c9c4175bc420362d155
Samfox2 <Samfox2@users.noreply.github.com>
2016-10-28 20:32:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ function DomotigaPlatform(log, config, api) {
 DomotigaPlatform.prototype.configureAccessory = function (accessory) {
     var accessoryId = accessory.context.name;
 
-    this.setService(accessory);
+    //this.setService(accessory);
     this.accessories[accessoryId] = accessory;
 }
 
