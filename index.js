f4aa32531ee83c6a644e69799d49deb49c8116bb
Samfox2 <Samfox2@users.noreply.github.com>
2018-03-07 12:28:25 +0100

    Update index.js
    temperature logging added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var JSONRequest = require("jsonrequest");
 var inherits = require('util').inherits;
 var path = require('path');
 var fs = require('fs');
+var moment = require('moment');
+var FakeGatoHistoryService = require('./fakegato-history.js')(this.platform.homebridge);
 
 module.exports = function (homebridge) {
     console.log("homebridge API version: " + homebridge.version);
@@ -453,7 +455,14 @@ DomotigaPlatform.prototype.addAccessory = function (data) {
 
         // Setup HomeKit switch service
         accessory.addService(primaryservice, data.name);
-
+	    
+        // Add history logging service    
+        if (accessory.context.valueTemperature && (accessory.context.service === "TemperatureSensor") ) {
+            this.log.debug("Adding Log Service for %s",accessory.context.name);
+            this.loggingService = new FakeGatoHistoryService("thermo", this, {storage: 'fs', path: this.platform.localCache,disableTimer:true});
+            accessory.addService(this.loggingService, data.name);
+        }    
+	    
         // New accessory is always reachable
         accessory.reachable = true;
 
@@ -537,6 +546,7 @@ DomotigaPlatform.prototype.doPolling = function (name) {
                 if (!error && value !== thisDevice.cacheCurrentTemperature) {
                     thisDevice.cacheCurrentTemperature = value;
                     primaryservice.getCharacteristic(Characteristic.CurrentTemperature).getValue();
+                    accessory.getService(Service.FakeGatoHistoryService).addEntry({time: moment().unix(), thisDevice.cacheCurrentTemperature});
                 }
             });
             break;
